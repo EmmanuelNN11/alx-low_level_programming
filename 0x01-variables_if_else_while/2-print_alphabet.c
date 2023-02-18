732087eb58ea355faed90dbd67bb443e18927404
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-02-17 17:03:39 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this code prints out the alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
