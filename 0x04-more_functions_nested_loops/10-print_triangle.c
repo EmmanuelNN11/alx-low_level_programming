58b68acc092ab86dec87faf5c99998a9cb155343
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-02-23 14:25:59 -0800

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle of squares
+ * @size: The size of the square
+ * Return: Empty
+ */
+
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = size - a; b > 1; b--)
+			{
+				_putchar(32);
+			}
+			for (c = 0; c <= a; c++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
