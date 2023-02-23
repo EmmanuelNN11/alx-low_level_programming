a10f7d17729a97e7b981c9921a1e174c1cf712dd
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-02-23 13:27:33 -0800

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - this code prints out n squares
+ * @size: the number of squares to be printed
+ * Return: Empty
+ */
+
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
