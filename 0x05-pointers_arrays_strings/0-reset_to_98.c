272e550f8b8259230a69b66f535d1384b15e05db
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-02-27 04:12:04 -0800

    updating stored character through a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this code updates the value of an int to 98
+ * @n: character to be updated
+ * Return: n if successful
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
