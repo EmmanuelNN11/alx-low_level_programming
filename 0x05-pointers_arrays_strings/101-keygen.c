85ded4dc9211ed5cca1a13c6a6ff8740946e93a7
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-02-27 16:28:13 -0800

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random passwords
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int password[100];
+	int a;
+	int sum;
+	int b;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (a = 0; a < 100; a++)
+	{
+		password[a] = rand() % 78;
+		sum += (password[a] + '0');
+		putchar(password[a] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			b = 2772 - sum - '0';
+			sum += b;
+			putchar(b + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
