0f4914d9c653c279cd3c6604759041c98456ecc7
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-03-08 06:30:40 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints out the length of a string
+ * @s: string to be tested
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
