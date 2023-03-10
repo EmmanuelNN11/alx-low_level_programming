58c5fd5328af2572ceb9908c38d5e50c51d3c169
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-03-10 05:24:20 -0800

    task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,15 +1,16 @@
 #include "main.h"
-#include<stdio.h>
-
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - this code prints out a string to standard output
+ * @str: string to be output
+ * Return: none negative number of successful
  */
 
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
