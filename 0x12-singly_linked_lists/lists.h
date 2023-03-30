bd7f98411965276a1bedb3527693ade3cc114bc5
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-03-30 15:10:18 -0700

    task 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+* Desc: header file containinf prototypes and definitions
+*	for all functions and types written in this proj
+*/
+
+#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,7 +15,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *              for Holberton project
  */
 typedef struct list_s
 {
