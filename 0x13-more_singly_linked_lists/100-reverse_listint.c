ef444f36acd974b9a7fc19794cc1714fa941929e
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-04-04 23:56:28 -0700

    task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,17 +11,17 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
+	listint_t *previous = NULL;
 	listint_t *next = NULL;
 
 	while (*head)
 	{
 		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
+		(*head)->next = previous;
+		previous = *head;
 		*head = next;
 	}
-	*head = prev;
+	*head = previous;
 
 	return (*head);
 }
