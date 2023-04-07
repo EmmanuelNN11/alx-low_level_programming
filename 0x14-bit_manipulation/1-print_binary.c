23474e989687aa975da23cb1c7bdb3e9ce425dd2
Emmanuel Nyandoro <emmanuelnyandoro40@gmail.com>
2023-04-06 21:39:16 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_binary - this code converts a number to binary
+ * @n: number to be tested
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
+
+}
